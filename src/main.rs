ecb92b49c6b28ef92fb25dc9ce9cfac85b738001
Ludovic Chopin <lchopin+github@gmail.com>
2022-11-19 17:51:59 +0100

    Try to add producer in the process, issue is not reproduced

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,33 @@ mod dbus_trait;
 
 use crate::dbus_trait::ProducerProxyAsync;
 use async_std::stream::StreamExt;
+use async_std::task;
 use std::error::Error;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
-use zbus::ConnectionBuilder;
+use zbus::{dbus_interface, ConnectionBuilder, SignalContext};
 
 pub struct AdaptStruct {}
+
+const BUS_NAME_PRODUCER: &str = "ludo_ic.daemon.producer";
+const INTERFACE_NAME_PRODUCER: &str = "/ludo_ic/daemon/producer";
+const INTERNAL_TIMER: u64 = 1;
+
+struct Greeter {}
+
+#[dbus_interface(name = "ludo_ic.daemon.producer")]
+impl Greeter {
+    async fn say_hello(&self, name: &str) -> String {
+        format!("Hello {}!", name)
+    }
+
+    #[dbus_interface(signal)]
+    async fn MySignalEvent(
+        ctxt: &SignalContext<'_>,
+        val1: i32,
+        val2: i32,
+    ) -> Result<(), zbus::Error>;
+}
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -33,6 +54,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .finish()
         .init();
 
+    // spawn producer task
+    task::spawn(async move {
+        let greeter = Greeter {};
+        let conn = ConnectionBuilder::session()
+            .unwrap()
+            .name(BUS_NAME_PRODUCER)?
+            .serve_at(INTERFACE_NAME_PRODUCER, greeter)
+            .unwrap()
+            .build()
+            .await
+            .unwrap();
+
+        let iface = conn
+            .object_server()
+            .interface::<_, Greeter>(INTERFACE_NAME_PRODUCER)
+            .await
+            .unwrap();
+        let sc = iface.signal_context();
+
+        loop {
+            async_std::task::sleep(std::time::Duration::from_millis(INTERNAL_TIMER)).await;
+            //println!("unblocked !");
+            Greeter::MySignalEvent(sc, 1, 43).await.unwrap();
+        }
+    });
+
     let dbus_conn = ConnectionBuilder::session()?.build().await?;
 
     let dbus_conn_listener = dbus_conn.clone();
@@ -40,8 +87,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let proxy = ProducerProxyAsync::builder(&dbus_conn_listener)
             .cache_properties(zbus::CacheProperties::No)
             .build()
-            .await
-            .unwrap();
+            .await?;
         let mut stream = proxy.receive_my_signal_event().await.unwrap();
         let _ = stream.next().await.unwrap();
         error!("Signal received."); // So it is visible
